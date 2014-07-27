ac7707c6791a63f55e595fcf68357214b81cd5f9
git-username <git-email-address>
2014-07-26 20:43:52 -0400

    added another route

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,18 @@
-angular.module('OWMApp',['ngRoute'])
-	.config(function($routeProvider){
-		$routeProvider.when('/', {
+angular.module('OWMApp',['ngRoute']).
+	config(function($routeProvider){
+		$routeProvider.
+		when('/', {
 			templateUrl : './home.html',
 			controller : 'HomeCtrl'
+		}).
+		when('/city',{
+			templateUrl : './city.html',
+			controller : 'CityCtrl'
 		});
-	})
-	.controller('HomeCtrl',function($scope){
+	}).
+	controller('HomeCtrl',function($scope){
 
+	}).
+	controller('CityCtrl',function($scope){
+		
 	});
